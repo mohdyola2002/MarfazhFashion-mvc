2045ca5f85363dbc59f6aff461bce9b8283aeb36
Muhammad Abdulkadir Yola <mohdyola2002@gmail.com>
2020-06-13 17:59:36 +0100

    Annotations to customer name in customer class

---
+++ Migrations/202006131657535_AnnotationsToCustomerName.cs
@@ -0,0 +1,18 @@
+namespace MarfazahFashion.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AnnotationsToCustomerName : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Customers", "Name", c => c.String(nullable: false, maxLength: 250));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Customers", "Name", c => c.String());
+        }
+    }
+}
