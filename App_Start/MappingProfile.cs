7de204f0eb52880ada0d6c6209c70ad7f609dbe9
Muhammad Abdulkadir Yola <mohdyola2002@gmail.com>
2020-06-15 23:23:30 +0100

    Web Api: CustomersController, Auto Mapper

---
+++ App_Start/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using MarfazahFashion.Dtos;
+using MarfazahFashion.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MarfazahFashion.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+
+        }
+    }
+}
---
+++ Dtos/CustomerDto.cs
@@ -0,0 +1,25 @@
+﻿using MarfazahFashion.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MarfazahFashion.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        public string Name { get; set; }
+
+        [Min18IYearsIfARetailer]
+        public DateTime? BirthDate { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+    }
+}
