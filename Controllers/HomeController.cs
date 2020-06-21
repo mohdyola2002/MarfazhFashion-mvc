5bdf1a432b0ba1ad95e38e6fc2c0f1e596d25bee
Muhammad Abdulkadir Yola <mohdyola2002@gmail.com>
2020-06-21 12:43:35 +0100

    Performance optimization: Output Caching

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 
 namespace MarfazahFashion.Controllers
 {
     [AllowAnonymous]
     public class HomeController : Controller
     {
+        //To Insert output caching:
+        //[OutputCache(Duration = 50, Location = OutputCacheLocation.Client, VaryByParam = "*")]
+        //Remove output caching:
+        [OutputCache(Duration = 0, VaryByParam = "*", NoStore = true)]
         public ActionResult Index()
         {
             return View();
